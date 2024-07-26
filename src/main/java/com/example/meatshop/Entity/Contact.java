5c7dbf067cb7b0053702a92c7c21956daf5f831d
krishnaher0 <krishna613460@gmail.com>
2024-07-26 19:33:23 +0545

    Contact table added for the storing contact details

---
+++ src/main/java/com/example/meatshop/Entity/Contact.java
@@ -0,0 +1,22 @@
+package com.example.meatshop.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
