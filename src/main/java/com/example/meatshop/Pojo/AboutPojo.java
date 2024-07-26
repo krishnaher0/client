736f6941a2f6bd1bf474c59a56339b77b0ea8a63
krishnaher0 <krishna613460@gmail.com>
2024-07-26 07:30:07 +0545

    Validation logic added for the about table data

---
+++ src/main/java/com/example/meatshop/Pojo/AboutPojo.java
@@ -0,0 +1,17 @@
+package com.example.meatshop.Pojo;
+
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AboutPojo {
+    private Integer aboutId;
+    private String OurMission;
+    private String OurVision;
+}
